b53574923dc78fd2a24cddfb885e78c8afd47627
ozodbek <muazzin769698@gmail.com>
2023-06-02 12:29:03 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <header className='shadow' >
+        <div className="container-fluid py-4">
+          
+          <img src="https://olcha.uz/_nuxt/logo-red.e48e0ab8.svg" alt="" />
+        </div>
+       </header>
+       <section id='products' className='py-3'>
+        <div className="container-fluid">
+          <div className="row">
+            <div className='col-sm-6 col-md-4 col-lg-3 productCol'>
+             <div className="praductCard p-3">
+             <img className='d-block productImg' src="https://olcha.uz/image/266x266/products/supplier/stores/1/2023-05-25/6kKrKI54YmPZG20U1y02GaIiAHkOwAq2kLOZC9bUNwUWNXjiYK6UfBPFDY3v.jpg" alt="" />
+              <a href="#" className='groupName d-block mb-3 mt-3'>Smartfon</a>
+              <p className='price mb-2'>2 290 000 sum</p>
+              <p className='priceMonth'>220 sum/month</p>
+              <p className='title'>samsung A52</p>
+              <a href="#" className='btn'>Sotib olish</a>
+           
+             </div>
+              </div>
+              <div className='col-sm-6 col-md-4 col-lg-3 productCol'>
+             <div className="praductCard">
+             <img className='d-block productImg' src="https://olcha.uz/image/266x266/products/supplier/stores/1/2023-05-25/6kKrKI54YmPZG20U1y02GaIiAHkOwAq2kLOZC9bUNwUWNXjiYK6UfBPFDY3v.jpg" alt="" />
+              <a href="#" className='groupName'>Smartfon</a>
+              <p className='price'>2 290 000 sum</p>
+              <p className='priceMonth'>220 sum/month</p>
+              <p className='title'>samsung A52</p>
+              <a href="#" className='btn'>Sotib olish</a>
+           
+             </div>
+              </div>
+              <div className='col-sm-6 col-md-4 col-lg-3 productCol'>
+             <div className="praductCard">
+             <img className='d-block productImg' src="https://olcha.uz/image/266x266/products/supplier/stores/1/2023-05-25/6kKrKI54YmPZG20U1y02GaIiAHkOwAq2kLOZC9bUNwUWNXjiYK6UfBPFDY3v.jpg" alt="" />
+              <a href="#" className='groupName'>Smartfon</a>
+              <p className='price'>2 290 000 sum</p>
+              <p className='priceMonth'>220 sum/month</p>
+              <p className='title'>samsung A52</p>
+              <a href="#" className='btn'>Sotib olish</a>
+           
+             </div>
+              </div>
+              <div className='col-sm-6 col-md-4 col-lg-3 productCol'>
+             <div className="praductCard">
+             <img className='d-block productImg' src="https://olcha.uz/image/266x266/products/supplier/stores/1/2023-05-25/6kKrKI54YmPZG20U1y02GaIiAHkOwAq2kLOZC9bUNwUWNXjiYK6UfBPFDY3v.jpg" alt="" />
+              <a href="#" className='groupName'>Smartfon</a>
+              <p className='price'>2 290 000 sum</p>
+              <p className='priceMonth'>220 sum/month</p>
+              <p className='title'>samsung A52</p>
+              <a href="#" className='btn'>Sotib olish</a>
+           
+             </div>
+              </div>
+              <div className='col-sm-6 col-md-4 col-lg-3 productCol'>
+             <div className="praductCard">
+             <img className='d-block productImg' src="https://olcha.uz/image/266x266/products/supplier/stores/1/2023-05-25/6kKrKI54YmPZG20U1y02GaIiAHkOwAq2kLOZC9bUNwUWNXjiYK6UfBPFDY3v.jpg" alt="" />
+              <a href="#" className='groupName'>Smartfon</a>
+              <p className='price'>2 290 000 sum</p>
+              <p className='priceMonth'>220 sum/month</p>
+              <p className='title'>samsung A52</p>
+              <a href="#" className='btn'>Sotib olish</a>
+           
+             </div>
+              </div>
+          
+          </div>
+        </div>
+
+       </section>
+    </div>
+  );
+}
+
+export default App;
